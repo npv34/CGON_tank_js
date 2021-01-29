665e757994f12b88ddfdaf9fac3445716344370e
npv34 <phanvanluan611996@gmail.com>
2021-01-29 19:40:54 +0700

    demo tank

---
+++ my.js
@@ -0,0 +1,22 @@
+function moveUp() {
+    let topCurrentTank =  parseInt(document.getElementById('tank').style.top);
+    document.getElementById('tank').style.top = topCurrentTank - 5 + "px"
+}
+
+function moveLeft() {
+    let leftCurrentTank =  parseInt(document.getElementById('tank').style.left);
+    document.getElementById('tank').style.left = leftCurrentTank - 5 + "px"
+
+}
+
+
+function moveDown() {
+
+
+}
+
+
+function moveRight() {
+
+    
+}
